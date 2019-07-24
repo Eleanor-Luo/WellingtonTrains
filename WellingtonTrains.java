33ecbbf16a0faf22037563f4c4b0b704d863f227
Joe Smith <smithjose4@myvuw.ac.nz>
2019-07-24 13:01:57 +1200

    first file upload

---
+++ WellingtonTrains.java
@@ -0,0 +1,138 @@
+// This program is copyright VUW.
+// You are granted permission to use it to construct your answer to a COMP103 assignment.
+// You may not distribute it in any other way without permission.
+
+/* Code for COMP103 - 2019T2, Assignment 2
+ * Name:Joe SMith
+ * Username:smithjose4
+ * ID:300488105
+ */
+
+import ecs100.*;
+import java.awt.Color;
+import java.util.*;
+import java.io.*;
+
+/**
+ * WellingtonTrains
+ * A program to answer queries about Wellington train lines and timetables for
+ *  the train services on those train lines.
+ * 
+ * See the assignment page for a description of the program and what you have to do.
+ */
+
+public class WellingtonTrains{
+    //Fields to store the collections of Stations and Lines
+    /*# YOUR CODE HERE */
+
+    //hash map created to store all the stations info in it the key is the name as all stations have different names
+    Map<String,Station> stationsMap = new HashMap<>();
+
+    // Fields for the suggested GUI.
+    private String stationName;            // station to get info about, or to start journey from
+    private String lineName;               // train line to get info about.
+    private String destinationName;  
+    private int startTime = 0;                  // time for enquiring about
+
+    /**
+     * main method:  load the data and set up the user interface
+     */
+    public static void main(String[] args){
+        WellingtonTrains wel = new WellingtonTrains();
+        wel.loadData();   // load all the data
+        wel.setupGUI();   // set up the interface
+    }
+
+    /**
+     * Load data files
+     */
+    public void loadData(){
+        loadStationData();
+        UI.println("Loaded Stations");
+        loadTrainLineData();
+        UI.println("Loaded Train Lines");
+        // The following is only needed for the Completion and Challenge
+        loadTrainServicesData();
+        UI.println("Loaded Train Services");
+    }
+
+    /**
+     * User interface has buttons for the queries and text fields to enter stations and and train line
+     * You will need to implement the methods here.
+     */
+    public void setupGUI(){
+        UI.addButton("All Stations",   this::listAllStations);
+        UI.addButton("All Lines",      this::listAllTrainLines);
+        UI.addTextField("Station",     (String name) -> {this.stationName=name;});
+        UI.addTextField("Train Line",  (String name) -> {this.lineName=name;});
+        UI.addTextField("Destination", (String name) -> {this.destinationName=name;});
+        UI.addTextField("Time (24hr)", (String time) ->
+            {try{this.startTime=Integer.parseInt(time);}catch(Exception e){UI.println("Enter four digits");}});
+        UI.addButton("Lines of Station",    () -> {listLinesOfStation(this.stationName);});
+        UI.addButton("Stations on Line",    () -> {listStationsOnLine(this.lineName);});
+        UI.addButton("Stations connected?", () -> {checkConnected(this.stationName, this.destinationName);});
+        UI.addButton("Next Services",       () -> {findNextServices(this.stationName, this.startTime);});
+        UI.addButton("Find Trip",           () -> {findTrip(this.stationName, this.destinationName, this.startTime);});
+        UI.addButton("Quit", UI::quit);
+        UI.setDivider(1.0);
+    }
+
+    // Methods for loading data and answering queries
+
+    /*# YOUR CODE HERE */
+
+    /**
+     * loads all the info about the train stations from the stations data file
+     * stores each station in the stationsMap and creates the station object to store in it
+     * */
+    public void loadStationData(){
+
+        //getting each line of the stations data file each line is related to a station
+        ArrayList<String> stationInfo = readAllLines("stations.data");
+
+        Scanner stationScan = null;
+        String stationName;
+        int zone;
+        float distFromHub;
+
+        //looping over each line
+        for(String station : stationInfo){
+            stationScan = new Scanner(station);
+
+            //get all the tokens from the line
+            while (stationScan.hasNextLine()){
+
+                //getting the name then zone then distance
+                stationName = stationScan.next();
+                zone = stationScan.nextInt();
+                distFromHub = stationScan.nextFloat();
+
+                //adding each station to the map using the name as the key and the station object as value
+                stationsMap.put(stationName,new Station(stationName,zone,distFromHub));
+            }
+
+        }
+
+        //if the scanner was used close it
+        if(stationScan != null) {
+            stationScan.close();
+        }
+
+    }
+
+    // Utility method to help with reading data files
+    /** Read all lines in a file into a list of Strings */
+    public ArrayList<String> readAllLines(String filename){
+        try{
+            ArrayList<String> ans = new ArrayList<String>();
+            Scanner sc = new Scanner(new File(filename));
+            while (sc.hasNext()){
+                ans.add(sc.nextLine());
+            }
+            sc.close();
+            return ans;
+        }
+        catch(IOException e){UI.println("Fail: " + e); return null;}
+    }
+
+}
