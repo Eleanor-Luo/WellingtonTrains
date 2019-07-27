d3ac0e0cfb312eb880ba9baad2be355e9ce47411
Joe Smith <smithjose4@myvuw.ac.nz>
2019-07-27 17:04:10 +1200

    added the TrainServices loading

--- WellingtonTrains.java
+++ WellingtonTrains.java
@@ -9,7 +9,10 @@
  */
 
 import ecs100.*;
+import javafx.beans.property.MapProperty;
+
 import java.awt.Color;
+import java.lang.reflect.Array;
 import java.util.*;
 import java.io.*;
 
@@ -28,6 +31,8 @@ public class WellingtonTrains{
     //hash map created to store all the stations info in it the key is the name as all stations have different names
     Map<String,Station> stationsMap = new HashMap<>();
 
+    //hash map for the train lines uses the key of its name and stores the object as value
+    Map<String, TrainLine> linesMap = new HashMap<>();
     // Fields for the suggested GUI.
     private String stationName;            // station to get info about, or to start journey from
     private String lineName;               // train line to get info about.
@@ -71,8 +76,9 @@ public class WellingtonTrains{
         UI.addButton("Lines of Station",    () -> {listLinesOfStation(this.stationName);});
         UI.addButton("Stations on Line",    () -> {listStationsOnLine(this.lineName);});
         UI.addButton("Stations connected?", () -> {checkConnected(this.stationName, this.destinationName);});
-        UI.addButton("Next Services",       () -> {findNextServices(this.stationName, this.startTime);});
-        UI.addButton("Find Trip",           () -> {findTrip(this.stationName, this.destinationName, this.startTime);});
+       // UI.addButton("Next Services",       () -> {findNextServices(this.stationName, this.startTime);});
+        //UI.addButton("Find Trip",           () -> {findTrip(this.stationName, this.destinationName, this.startTime);});
+        UI.addButton("Clear", UI::clearText);
         UI.addButton("Quit", UI::quit);
         UI.setDivider(1.0);
     }
@@ -88,34 +94,291 @@ public class WellingtonTrains{
     public void loadStationData(){
 
         //getting each line of the stations data file each line is related to a station
-        ArrayList<String> stationInfo = readAllLines("stations.data");
+        ArrayList<String> stationInfo = readAllLines(".\\data\\stations.data");
 
         Scanner stationScan = null;
         String stationName;
         int zone;
         float distFromHub;
 
-        //looping over each line
-        for(String station : stationInfo){
-            stationScan = new Scanner(station);
+        //try catch as scanner might cause error incase format changes
+        try {
 
-            //get all the tokens from the line
-            while (stationScan.hasNextLine()){
+            if(!stationInfo.isEmpty()) {
 
-                //getting the name then zone then distance
-                stationName = stationScan.next();
-                zone = stationScan.nextInt();
-                distFromHub = stationScan.nextFloat();
+                //looping over each line
+                for (String station : stationInfo) {
+                    stationScan = new Scanner(station);
 
-                //adding each station to the map using the name as the key and the station object as value
-                stationsMap.put(stationName,new Station(stationName,zone,distFromHub));
+                    //get all the tokens from the line
+                    while (stationScan.hasNext()) {
+
+                        //getting the name then zone then distance
+                        stationName = stationScan.next();
+                        zone = stationScan.nextInt();
+                        distFromHub = stationScan.nextFloat();
+
+                        //adding each station to the map using the name as the key and the station object as value
+                        stationsMap.put(stationName, new Station(stationName, zone, distFromHub));
+                    }
+
+                }
             }
+        }
+        catch(Exception e){
 
+            //as stations cant be loaded must close the program
+            UI.println("Error reading stations file (incorrect format)");
+            UI.sleep(2000);
+            UI.quit();
         }
 
         //if the scanner was used close it
         if(stationScan != null) {
             stationScan.close();
+        }
+
+    }
+
+    /**
+     * loads all the train lines into the train line map
+     * also reads each related stations data file for each
+     * line this allows the stations list in the line object to be
+     * filled and the lines list on the stations object to be filled
+     * */
+    public void loadTrainLineData(){
+
+        //getting all the lines from the train lines data file
+        ArrayList<String> trainLinesInfo = readAllLines(".\\data\\train-lines.data");
+        ArrayList<String> linesStations;
+
+        if(!trainLinesInfo.isEmpty()) {
+
+
+            //for each line create a new trainline object and add it to the map each line only has the name so no scanner needed
+            for (String trainLine : trainLinesInfo) {
+
+                //adding the line to the lines map and creating object
+                linesMap.put(trainLine, new TrainLine(trainLine));
+
+                //reading all the stations that this train passes through
+                linesStations = readAllLines(".\\data\\" + trainLine + "-stations.data");
+
+                if(!linesStations.isEmpty()) {
+
+                    for (String station : linesStations) {
+
+                        //if the station is real
+                        if (stationsMap.containsKey(station)) {
+
+                            //add the station object to the lines station list and the line to the stations line list
+                            linesMap.get(trainLine).addStation(stationsMap.get(station));
+                            stationsMap.get(station).addTrainLine(linesMap.get(trainLine));
+
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+
+    /**
+     * loads the services times for each train line each time is added to the trainService object
+     * and then that is added to the trainServices Collection on the related line object
+     *
+     * */
+    public void loadTrainServicesData(){
+
+
+        ArrayList<String> times;
+        Scanner sc = null;
+
+        //looping over all line names (key)
+        for(String tL : linesMap.keySet()){
+
+            //reading the services file for that line
+            times = readAllLines(".\\data\\" + tL + "-services.data");
+
+            //make sure safe to loop
+            if(!times.isEmpty()) {
+
+                //looping over each line in the file
+                for (String s : times) {
+
+                    //making a temp servive for the current line looped over so can add times then add to the line
+                    TrainService tempService = new TrainService(linesMap.get(tL));
+
+                    //dealing with scanner and format could be wrong
+                    try {
+
+                        sc = new Scanner(s);
+
+                        //adding all the times on the current line of file to the temperary service object
+                        while (sc.hasNext()) {
+
+                            tempService.addTime(sc.nextInt());
+                        }
+
+                    } catch (Exception e) {
+                        UI.println("error reading times wrong format");
+                    }
+
+                    //all times added so add the service to the lines trainService list
+                    linesMap.get(tL).addTrainService(tempService);
+                }
+            }
+        }
+
+        //closing scanner
+        if(sc != null){
+            sc.close();
+        }
+    }
+
+    /**
+     * querry 1 this will list to the user all of the stations names from the stations map
+     * as the names are the keys it will print all the keys
+     * */
+    public void listAllStations(){
+
+        //cant loop on empty map
+        if(!stationsMap.isEmpty()) {
+
+            //looping over all the keys (station names) and printing them to the screan
+            for (String s : stationsMap.keySet()) {
+                UI.println(s);
+            }
+        }
+        else{
+            UI.println("no stations avalible (not loaded)");
+        }
+    }
+
+    /**
+     * querry 2 this will list the names of the train lines by printing all the keys as they are the names
+     * */
+    public void listAllTrainLines(){
+
+        //cant loop on empty map
+        if(!linesMap.isEmpty()) {
+
+            //looping over all the keys (line names) and printing them to screen
+            for (String l : linesMap.keySet()) {
+                UI.println(l);
+            }
+        }
+        else{
+            UI.println("no Lines avalible (not loaded)");
+        }
+
+    }
+
+
+
+    /**
+     * query 3
+     * takes a station name as parameter
+     * lists all the lines that go through the selected station
+     * @param stationN
+     * */
+    public void listLinesOfStation(String stationN){
+
+        //making sure the station to check exists
+        if(stationsMap.containsKey(stationN)) {
+
+            //storing the selected station in a objecct
+            Station selectedStation = stationsMap.get(stationN);
+
+            //getting all the lines that go through that station into a set
+            Set<TrainLine> linesThroughStation = selectedStation.getTrainLines();
+
+            //listing each lines name that is in that set of lines
+            for (TrainLine tL : linesThroughStation) {
+
+                UI.println(tL.getName());
+            }
+        }
+        else{
+            UI.println("Station doesnt exist make sure to use - for spaces");
+        }
+
+    }
+
+    /**
+     * 4th querry
+     * takes the line to get the stations on it as parameter
+     * then lists all the stations that are on this line
+     *
+     * @param lineN
+     * */
+    public void listStationsOnLine(String lineN){
+
+        //making sure the line to check for stations exists
+        if(linesMap.containsKey(lineN)) {
+
+            //storing the selected trainline as an object
+            TrainLine selectedLine = linesMap.get(lineN);
+
+            //getting all the stations that are on that line
+            List<Station> stationsOnline = selectedLine.getStations();
+
+            //printing each station name to the screen
+            for (Station s : stationsOnline) {
+
+                UI.println(s.getName());
+            }
+        }
+        else{
+            UI.println("line doesnt exist dont forget - for spaces and _ to seperate first and final station on line");
+        }
+
+    }
+
+
+    /**
+     * prints to screen the lines that has the selected station and the destination station
+     * heading in the correect direction
+     * searches each line to check if it has the stations in correct order
+     * @param stationN
+     * @param destN
+     * */
+    public void checkConnected(String stationN ,String destN ){
+
+        //making sure both stations exist
+        if(stationsMap.containsKey(stationN) && stationsMap.containsKey(destN)) {
+
+            boolean hasDepart;
+
+            //looping over each line
+            for (TrainLine tL : linesMap.values()) {
+
+                //set to false to make sure not true from last line check
+                hasDepart = false;
+
+                //getting all stations on that line
+                List<Station> linesStations = tL.getStations();
+
+                if(!linesStations.isEmpty()) {
+                    for (Station s : linesStations) {
+
+                        //making sure that the station name is before the destination in the list so the order isnt messed up
+                        if (s.getName().equals(stationN)) {
+
+                            //set to true so can now check for the destination station
+                            hasDepart = true;
+                        } else if (hasDepart && s.getName().equals(destN)) {
+
+                            UI.println(tL.getName());
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        else{
+
+            UI.println("Stations must exist dont forget - for spaces");
         }
 
     }
